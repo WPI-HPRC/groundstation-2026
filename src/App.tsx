ccaedb81c609b9d21cce70dbf97c511e85e9787f
Nic DeBruin <github@ndebruin.com>
2026-04-25 02:45:49 -0400

    base state. it builds

--- src/App.tsx
+++ src/App.tsx
@@ -18,13 +18,13 @@ function App() {
 
       <div className="row">
         <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+          {/* <img src="/vite.svg" className="logo vite" alt="Vite logo" /> */}
         </a>
         <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+          {/* <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" /> */}
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
         </a>
       </div>
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
